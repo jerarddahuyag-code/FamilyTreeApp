f2fc9c0ea3edba0c77f781f892ec317c17201b38
jkdahuyag <jerard.dahuyag@gmail.com>
2026-07-23 20:28:24 +0800

    added base google oidc

--- FamilyTreeApp/FamilyTreeApp.Api/Program.cs
+++ FamilyTreeApp/FamilyTreeApp.Api/Program.cs
@@ -1,6 +1,8 @@
 using FamilyTreeApp.Api.Middleware;
 using FamilyTreeApp.Application;
 using FamilyTreeApp.Infrastructure;
+using Google.Apis.Auth.AspNetCore3;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Text.Json.Serialization;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -16,6 +18,19 @@ builder.Services.AddControllers()
     {
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     }); ;
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddGoogleOpenIdConnect(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 
